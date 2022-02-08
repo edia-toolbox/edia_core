9f76d8437ee8ccc3401733697a320c50f1461701
p.jdemooi <jeroen@the1stfloor.net>
2022-02-08 21:17:52 +0100

    Copy

--- Assets/eDIA/Runtime/e.IO/FileManager.cs
+++ Assets/eDIA/Runtime/e.IO/FileManager.cs
@@ -93,6 +93,35 @@ namespace eDIA {
 			return result;
 		}
 
+		public static void Copy(string sourceDirectory, string targetDirectory)
+		{
+			var diSource = new DirectoryInfo(sourceDirectory);
+			var diTarget = new DirectoryInfo(targetDirectory);
+
+			CopyAll(diSource, diTarget);
+		}
+
+		public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+		{
+			Directory.CreateDirectory(target.FullName);
+
+			// Copy each file into the new directory.
+			foreach (FileInfo fi in source.GetFiles())
+			{
+				Debug.Log((@"Copying {0}\{1}", target.FullName, fi.Name));
+				fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+			}
+
+			// Copy each subdirectory using recursion.
+			foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+			{
+				DirectoryInfo nextTargetSubDir =
+				target.CreateSubdirectory(diSourceSubDir.Name);
+				CopyAll(diSourceSubDir, nextTargetSubDir);
+			}
+		}
+
+		
 		public static string ReadStringFromApplicationPathSubfolder (string _subfolder, string _fileName) {
 			string path =  CorrectPath() + "/" + _subfolder + "/" + _fileName;
 
