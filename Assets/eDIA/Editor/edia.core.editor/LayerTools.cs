6d73cfc33e309fef458fc52813c27d3866bd99c8
Jeroen De mooij <jeroen@the1stfloor.net>
2023-12-18 21:44:49 +0100

    LayerTools refactor

--- Assets/eDIA/Editor/edia.core.editor/LayerTools.cs
+++ Assets/eDIA/Editor/edia.core.editor/LayerTools.cs
@@ -27,6 +27,9 @@ namespace eDIA.EditorUtils
 
 				serializedObject.ApplyModifiedProperties();
 				serializedObject.Update();
+			} else {
+				Debug.LogError("TagManager.asset not loaded");
+				return;
 			}
 
 			Debug.Log("<color=#00FFFF>[eDIA]</color> Done");
@@ -35,50 +38,26 @@ namespace eDIA.EditorUtils
 		static void AddLayerAt(SerializedProperty layers, int index, string layerName, bool tryOtherIndex = true)
 		{
 			// Skip if a layer with the name already exists.
-			for (int i = 0; i < layers.arraySize; ++i)
+			for (int i = 0; i < layers.arraySize; i++)
 			{
 				if (layers.GetArrayElementAtIndex(i).stringValue == layerName)
 				{
-					Debug.Log("Skipping layer '" + layerName + "' because it already exists.");
+					Debug.Log("<i>" + layerName + " </i> already exists on index " + i + ", skipping");
 					return;
 				}
 			}
 
-			// Extend layers if necessary
-			if (index >= layers.arraySize)
-				layers.arraySize = index + 1;
-
 			// set layer name at index
 			var element = layers.GetArrayElementAtIndex(index);
+
 			if (string.IsNullOrEmpty(element.stringValue))
 			{
 				element.stringValue = layerName;
-				Debug.Log("Added layer '" + layerName + "' at index " + index + ".");
+				Debug.Log(layerName + " added on index " + index);
 			}
 			else
 			{
-				Debug.LogWarning("Could not add layer at index " + index + " because there already is another layer '" + element.stringValue + "'.");
-
-				if (tryOtherIndex)
-				{
-					// Go up in layer indices and try to find an empty spot.
-					for (int i = index + 1; i < 32; ++i)
-					{
-						// Extend layers if necessary
-						if (i >= layers.arraySize)
-							layers.arraySize = i + 1;
-
-						element = layers.GetArrayElementAtIndex(i);
-						if (string.IsNullOrEmpty(element.stringValue))
-						{
-							element.stringValue = layerName;
-							Debug.Log("Added layer '" + layerName + "' at index " + i + " instead of " + index + ".");
-							return;
-						}
-					}
-
-					Debug.LogError("Could not add layer " + layerName + " because there is no space left in the layers array.");
-				}
+				Debug.LogError("Creating <i>" + layerName + "</i> on " + index + " failed. Layer contains: <i>" + element.stringValue + "</i>. Please reassign your objects to a layer between 11-30");
 			}
 		}
 
