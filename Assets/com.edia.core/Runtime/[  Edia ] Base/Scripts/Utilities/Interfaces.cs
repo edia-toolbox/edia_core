5fcc75362c93772c81f4afd57e848baf0bbd03c7
Felix Klotzsche <klotzsche@cbs.mpg.de>
2024-05-07 14:30:24 +0200

    add EyeID enum

--- Assets/com.edia.core/Runtime/[  Edia ] Base/Scripts/Utilities/Interfaces.cs
+++ Assets/com.edia.core/Runtime/[  Edia ] Base/Scripts/Utilities/Interfaces.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Edia;
 
-public interface ILslTimer {
-    public double GetTime();
-}
+namespace Edia {
+    public interface ILslTimer {
+        public double GetTime();
+    }
 
-public interface ILslPusher {
-    public void PushSample(float posX, float posY, float posZ,
-        float rotX, float rotY, float rotZ,
-        float pupilDiameter = 0f,
-        float confidence = 0f,
-        float timestampEt = 0f,
-        double timestampLsl = 0);
+    public interface ILslPusher {
+
+        public Constants.EyeId EyeId { get; set; }
+
+        public void PushSample(float posX, float posY, float posZ,
+            float rotX, float rotY, float rotZ,
+            float pupilDiameter = 0f,
+            float confidence = 0f,
+            float timestampEt = 0f,
+            double timestampLsl = 0);
+    }
 }
