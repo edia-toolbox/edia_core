9827aa6006afb8c28299ccd7f0ac875129239f26
Felix Klotzsche <klotzsche@cbs.mpg.de>
2024-06-18 19:44:01 +0200

    rename and refac IEyeOutlet and ILslTimeAccessible

--- Assets/com.edia.core/Runtime/[  Edia ] Base/Scripts/Utilities/Interfaces.cs
+++ Assets/com.edia.core/Runtime/[  Edia ] Base/Scripts/Utilities/Interfaces.cs
@@ -1,25 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+///<summary>
+/// Interfaces used across EDIA Core and other EDIA packages.
+/// </summary>
+
 using UnityEngine;
-using Edia;
 
 namespace Edia {
-    public interface ILslPusher {
 
+    /// <summary>
+    /// Interface for pushing eye tracking data to an LSL (Lab Streaming Layer) stream.
+    /// Required in order not to need to make EDIA LSL a dependency for EDIA Eye.
+    /// </summary>
+    public interface ILslEyeOutlet : ILslTimeAccessible {
+
+        /// <summary>
+        /// Get or set the identifier for the eye (LEFT, RIGHT, CENTER).
+        /// </summary>
         public Constants.EyeId EyeId { get; set; }
 
-        public void PushSample(float posX, float posY, float posZ,
-            float rotX, float rotY, float rotZ,
-            float pupilDiameter = 0f,
-            float confidence = 0f,
-            float timestampEt = 0f,
-            double timestampLsl = 0);
-
-        public double GetTime();
+        ///<summary>
+        /// Builds and pushes a sample with eye tracking data.
+        /// </summary>
+        /// <param name="eyePositionLocal">The local eye position.</param>
+        /// <param name="eyeRotationLocalEuler">The local eye rotation in Euler Angles.</param>
+        /// <param name="pupilDiameter">The diameter of the pupil. Default is 0f.</param>
+        /// <param name="confidence">The confidence level of the eye tracking data. Default is 0f.</param>
+        /// <param name="timestampEt">The eye tracker timestamp. Default is 0f.</param>
+        /// <param name="timestampLsl">The LSL timestamp. Default is 0 and will timestamp the sample on sending.</param>
+        public void PushSample(Vector3 eyePositionLocal,
+                               Vector3 eyeRotationLocalEuler,
+                               float pupilDiameter = 0f,
+                               float confidence = 0f,
+                               float timestampEt = 0f,
+                               double timestampLsl = 0);
     }
 
-    public interface ILslTimer {
-        public double GetTime();
+
+    public interface ILslTimeAccessible {
+        public abstract double GetLslTime();
     }
 
 }
