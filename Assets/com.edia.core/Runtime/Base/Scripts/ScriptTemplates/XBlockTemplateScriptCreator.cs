3350589d6fc9600336970c3f8b992f65dcb9f8fe
Jeroen De mooij <jeroen@the1stfloor.net>
2025-05-04 22:32:43 +0200

    Add xblock template from Assets>Create>EDIA>XblockTemplate

---
+++ Assets/com.edia.core/Runtime/Base/Scripts/ScriptTemplates/XBlockTemplateScriptCreator.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+using System.IO;
+
+public static class XBlockTemplateScriptCreator
+{
+    private const string Template =
+@"using Edia;
+using UnityEngine;
+
+public class XBlockTemplate : XBlock
+{
+    // Add your implementation here.
+}";
+
+    [MenuItem("Assets/Create/EDIA/XBlockTemplate Script", false, 80)]
+    public static void CreateXBlockTemplateScript()
+    {
+        string path = GetSelectedPathOrFallback();
+        string scriptPath = Path.Combine(path, "NewXBlockTemplate.cs");
+        scriptPath = AssetDatabase.GenerateUniqueAssetPath(scriptPath);
+        File.WriteAllText(scriptPath, Template);
+        AssetDatabase.Refresh();
+        Selection.activeObject = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(scriptPath);
+    }
+
+    private static string GetSelectedPathOrFallback()
+    {
+        string path = "Assets";
+        foreach (var obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+        {
+            path = AssetDatabase.GetAssetPath(obj);
+            if (!string.IsNullOrEmpty(path) && File.Exists(path))
+            {
+                path = Path.GetDirectoryName(path);
+                break;
+            }
+        }
+        return path;
+    }
+}
