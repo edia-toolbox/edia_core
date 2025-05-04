72a281e21693800d309acd1195a320fafdf1d903
Jeroen De mooij <jeroen@the1stfloor.net>
2025-05-04 21:12:47 +0200

    Overwrite header logo only if empty

--- Assets/com.edia.core/Runtime/Controller/Scripts/PanelHeader.cs
+++ Assets/com.edia.core/Runtime/Controller/Scripts/PanelHeader.cs
@@ -1,50 +1,50 @@
-using Edia.Controller;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 namespace Edia {
 
-	public class PanelHeader : MonoBehaviour {
+    public class PanelHeader : MonoBehaviour {
 
-		public Image logo = null;
-		public TextMeshProUGUI titleField = null;
+        public Image           logo       = null;
+        public TextMeshProUGUI titleField = null;
 
-		void Awake () {
-			//! Set the control panel gameobject on correct layer
-			transform.root.gameObject.layer = LayerMask.NameToLayer("ControlUI");
-			SetLogo(Resources.Load<Sprite>("IconEdia"));
-		}
+        void Awake() {
+            //! Set the control panel gameobject on correct layer
+            transform.root.gameObject.layer = LayerMask.NameToLayer("ControlUI");
+            
+            if (logo.sprite is null) {
+                SetLogo(Resources.Load<Sprite>("IconEdia"));
+            }
+        }
 
-		void OnEnable() {
-			EventManager.StartListening(Edia.Events.ControlPanel.EvUpdateSessionSummary, OnEvUpdateSessionSummary );
-		}
+        void OnEnable() {
+            EventManager.StartListening(Edia.Events.ControlPanel.EvUpdateSessionSummary, OnEvUpdateSessionSummary);
+        }
 
-		private void OnDestroy() {
-			EventManager.StopListening(Edia.Events.ControlPanel.EvUpdateSessionSummary, OnEvUpdateSessionSummary );
-		}
+        private void OnDestroy() {
+            EventManager.StopListening(Edia.Events.ControlPanel.EvUpdateSessionSummary, OnEvUpdateSessionSummary);
+        }
 
-		public void LogoClicked() {
-			            
-			if (Edia.Controller.ControlPanel.Instance.ControlMode == Constants.ControlModes.Remote)
-				if (!Edia.Controller.ControlPanel.Instance.IsConnected)
-					EventManager.TriggerEvent(Edia.Events.ControlPanel.EvShowMessageBox, new eParam("Unable to retrieve settings, not connected", false));
-			
-			EventManager.TriggerEvent(Edia.Events.Settings.EvOpenSystemSettings);
-		}
+        public void LogoClicked() {
+            if (Edia.Controller.ControlPanel.Instance.ControlMode == Constants.ControlModes.Remote)
+                if (!Edia.Controller.ControlPanel.Instance.IsConnected)
+                    EventManager.TriggerEvent(Edia.Events.ControlPanel.EvShowMessageBox, new eParam("Unable to retrieve settings, not connected", false));
 
-		void OnEvUpdateSessionSummary(eParam obj)
-		{
-			SetTitle(obj.GetStrings()[0]);
-		}
+            EventManager.TriggerEvent(Edia.Events.Settings.EvOpenSystemSettings);
+        }
 
-		public void SetLogo (Sprite logoImage) {
-			logo.sprite = logoImage;
-		}
+        void OnEvUpdateSessionSummary(eParam obj) {
+            SetTitle(obj.GetStrings()[0]);
+        }
 
-		void SetTitle (string title) {
-			titleField.text = title;
-		}
+        public void SetLogo(Sprite logoImage) {
+            logo.sprite = logoImage;
+        }
 
-	}
+        void SetTitle(string title) {
+            titleField.text = title;
+        }
+
+    }
 }
